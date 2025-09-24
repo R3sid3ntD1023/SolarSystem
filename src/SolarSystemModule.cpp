4b302551ff5a0b04c10f85c0abf5573f01b26c17
dbee00 <dariusbigbee12@gmail.com>
2025-09-24 13:26:57 -0400

    updated solar system

---
+++ src/SolarSystemModule.cpp
@@ -0,0 +1,11 @@
+#include "SolarSystemModule.h"
+
+void SolarSystemModule::OnInitialize()
+{
+	LOG_INFO("Initialize Solar System Module");
+}
+
+void SolarSystemModule::OnShutdown()
+{
+	LOG_INFO("Shutdown Solar System Module");
+}
--- src/SolarSystemModule.h
+++ src/SolarSystemModule.h
@@ -1,3 +1,13 @@
 #pragma once
 
+#include "Module/Module.h"
+
 #define SOLAR_SYSTEM_API __declspec(dllexport)
+
+class SOLAR_SYSTEM_API SolarSystemModule : public BHive::Module
+{
+public:
+	void OnInitialize() override;
+
+	void OnShutdown() override;
+};
